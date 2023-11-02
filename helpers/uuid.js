58119e4230485d1e8996b652c7446148df20a6f6
mintyry <gitmintyry@gmail.com>
2023-11-01 21:00:48 -0700

    added uuid, unsure how to implement it into notes objects

---
+++ helpers/uuid.js
@@ -0,0 +1,13 @@
+// Immediately export a function that generates a string of random numbers and letters
+// module.exports = () =>
+//     Math.floor((1 + Math.random()) * 0x10000)
+//         .toString(16)
+//         .substring(1);
+
+function uuid() {
+    Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+}
+
+module.exports = uuid();
--- server.js
+++ server.js
@@ -7,10 +7,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const fs = require('fs');
 const path = require('path');
+const uuid = require('./helpers/uuid');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended : true }));
 app.use(express.static('public'));
+
+// GET * should return the index.html file. **We dont need to do this since it's handled by public, correct?
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'))
@@ -21,16 +24,30 @@ app.get('/api/notes', (req, res) => {
     res.json(existingNotes);
 })
 
+//needs to add uuid to each note
 app.post('/api/notes', (req, res) => {
-    console.log('line 25');
     const existingNotes = require('./db/db.json');
+    const { title, text } = req.body;
+
+    // // If all the required properties are present
+    // if (title && text) {
+    //   // Variable for the object we will save
+    //   const newNote = {
+    //     title,
+    //     text,
+    //     note_id: uuid(),
+    //   };
+
+    //   readAndAppend(newNote, './db/db.json');
+    
     existingNotes.push(req.body) //body is baked in; part of http requests; one of the properties of req for POST
-    fs.writeFile('./db/db.json', JSON.stringify(existingNotes, null, 2), err =>{ //fs needs to take in string
+    fs.writeFile('./db/db.json', JSON.stringify(existingNotes, null, 2), err => { //fs needs to take in string
         if(err){
             console.log(err)
         } else {
             console.log('Note saved!');
-            res.status(201).end(); //similar to json, send, sendFile
+            res.status(201).end(); //similar to json, send, sendFile; let front end know you dont have to keep waiting
+            //route handlers always have to have response; knew to use end bc connection needs to end for functions to run
         }
     })
 })
